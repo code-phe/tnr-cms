6da154da3b542e996e720f1ac7bace0052616aa8
tranlehaiquan <tranlehaiquan@gmail.com>
2022-08-28 16:33:49 +0700

    done config deploy heroku

--- config/database.js
+++ config/database.js
@@ -1,13 +1,24 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+const parse = require('pg-connection-string').parse;
+
+module.exports = ({ env }) => {
+  const config = parse(env('DATABASE_URL') || '');
+  console.log(config)
+
+  const connection = {
+    host: config.host || env('DATABASE_HOST', '127.0.0.1'),
+    port: config.port || env.int('DATABASE_PORT', 2345),
+    database: config.database || env('DATABASE_NAME', 'strapi'),
+    user: config.user || env('DATABASE_USERNAME', 'strapi'),
+    password: config.password || env('DATABASE_PASSWORD', 'strapi_admin'),
+    ssl: config.host ? {
+      rejectUnauthorized: false,
+    } : env.bool('DATABASE_SSL', false),
+  }
+
+  return {
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 2345),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi_admin'),
-      ssl: env.bool('DATABASE_SSL', false),
+      client: 'postgres',
+      connection,
     },
-  },
-});
+  };
+};
---
+++ src/api/request-contact/routes/request-contact.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * request-contact router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::request-contact.request-contact');
