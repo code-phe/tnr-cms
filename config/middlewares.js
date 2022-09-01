b12995d36aa64bfc1a60d821a79a9065e3b1753a
tranlehaiquan <tranlehaiquan@gmail.com>
2022-09-01 10:26:46 +0700

    Integration with s3

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,30 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
+  'strapi::errors',{
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'strapi-testing-images.s3.ap-southeast-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'strapi-testing-images.s3.ap-southeast-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
