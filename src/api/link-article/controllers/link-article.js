b46751c9ec892476352e6acb87a8c22a2db37121
tranlehaiquan <tranlehaiquan@gmail.com>
2023-01-25 18:59:42 +0700

    remove Modal not use

--- src/api/link-article/controllers/link-article.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * link-article controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::link-article.link-article');
--- src/api/link-external/services/link-external.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * link-external service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::link-external.link-external');
